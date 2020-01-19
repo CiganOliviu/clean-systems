b01e2d49a59e3392bd328e7e51e84261dec9a418
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-01-19 17:55:17 +0200

    Update matrixProcessProblem.cpp

--- matrixProcessProblem/matrixProcessProblem.cpp
+++ matrixProcessProblem/matrixProcessProblem.cpp
@@ -88,31 +88,31 @@ class dataProcessor {
 private:
 	validationRules __rules__;
 
-	template <class Type> bool isPrime (Type parameter);
+  template <class Type> bool isPrime (Type parameter);
   template <class Type> void interchangeValues (Type * parameterOne, Type * parameterTwo);
 
 public:
-	dataProcessor () {}
+  dataProcessor () {}
 
-	template <class Type> void readMatrix (char * fileName, matrixType<Type> & MTObject);
-	template <class Type> void putsMatrix (matrixType<Type> & MTObject);
-	template <class Type> void primeValuesFromMainDiagonal (matrixType<Type> & MTObject);
-	template <class Type> void paralelValuesWithMD (matrixType<Type> & MTObject);
-	template <class Type> void paralelValuesWithSD (matrixType<Type> & MTObject);
+  template <class Type> void readMatrix (char * fileName, matrixType<Type> & MTObject);
+  template <class Type> void putsMatrix (matrixType<Type> & MTObject);
+  template <class Type> void primeValuesFromMainDiagonal (matrixType<Type> & MTObject);
+  template <class Type> void paralelValuesWithMD (matrixType<Type> & MTObject);
+  template <class Type> void paralelValuesWithSD (matrixType<Type> & MTObject);
   template <class Type> void matrixTransposition (matrixType<Type> & MTObject);
 
-	virtual ~dataProcessor () {}
+  virtual ~dataProcessor () {}
 };
 
 template <class Type> bool dataProcessor::isPrime (Type parameter) {
 
-	if (parameter <= 0) return false;
-	if (parameter == 2) return true;
+  if (parameter <= 0) return false;
+  if (parameter == 2) return true;
 
-	for (size_t iterator = 2; iterator <= parameter / 2; iterator++)
-		if (parameter % iterator == 0) return false;
+  for (size_t iterator = 2; iterator <= parameter / 2; iterator++)
+    if (parameter % iterator == 0) return false;
 
-	return true;
+  return true;
 }
 
 template <class Type> void dataProcessor::interchangeValues (Type * parameterOne, Type * parameterTwo) {
@@ -157,60 +157,60 @@ template <class Type> void dataProcessor::readMatrix (char * fileName, matrixTyp
 
 template <class Type> void dataProcessor::putsMatrix (matrixType<Type> & MTObject) {
 
-	if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
-	if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
+  if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
+  if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
 
-	for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++) {
-		for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
-			std::cout << MTObject.matrix[iterator][jiterator] << " ";
-		std::cout << '\n';
-	}
+  for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++) {
+    for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
+      std::cout << MTObject.matrix[iterator][jiterator] << " ";
+    std::cout << '\n';
+   }
 }
 
 template <class Type> void dataProcessor::primeValuesFromMainDiagonal (matrixType<Type> & MTObject) {
 
-	if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
-	if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
+  if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
+  if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
 
-	for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
-		for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
-			if (iterator == jiterator)
-				if (isPrime(MTObject.matrix[iterator][jiterator])) std::cout << MTObject.matrix[iterator][jiterator] << " ";
-	std::cout << '\n';
+  for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
+    for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
+      if (iterator == jiterator)
+        if (isPrime(MTObject.matrix[iterator][jiterator])) std::cout << MTObject.matrix[iterator][jiterator] << " ";
+  std::cout << '\n';
 }
 
 template <class Type> void dataProcessor::paralelValuesWithMD (matrixType<Type> & MTObject) {
 
-	if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
-	if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
+  if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
+  if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
 
-	int order;
+  int order;
 
-	std::cin >> order;
+  std::cin >> order;
 
-	if (__rules__.isZero(order)) throw systemException ("Unable to process with order as zero");
+  if (__rules__.isZero(order)) throw systemException ("Unable to process with order as zero");
 
-	for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
-		for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
-			if (abs(iterator - jiterator) == order) std::cout << MTObject.matrix[iterator][jiterator] << " ";
-	std::cout << '\n';
+  for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
+    for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
+      if (abs(iterator - jiterator) == order) std::cout << MTObject.matrix[iterator][jiterator] << " ";
+  std::cout << '\n';
 }
 
 template <class Type> void dataProcessor::paralelValuesWithSD (matrixType<Type> & MTObject) {
 
-	if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
-	if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
+  if (__rules__.isZero(MTObject.lineRefference) || __rules__.isZero(MTObject.columnRefference)) throw systemException ("Unable to process with line or column as zero");
+  if (__rules__.isNegative(MTObject.lineRefference) || __rules__.isNegative(MTObject.columnRefference)) throw systemException ("Unable to process with negative line or column");
 
-	int order;
+  int order;
 
-	std::cin >> order;
+  std::cin >> order;
 
-	if (__rules__.isZero(order)) throw systemException ("Unable to process with order as zero");
+  if (__rules__.isZero(order)) throw systemException ("Unable to process with order as zero");
 
-	for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
-		for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
-			if (abs(iterator + jiterator - MTObject.line + 1) == order) std::cout << MTObject.matrix[iterator][jiterator] << " ";
-	std::cout << '\n';
+  for (size_t iterator = MTObject.startLinePoint; iterator < MTObject.lineRefference + MTObject.endLinePoint; iterator++)
+    for (size_t jiterator = MTObject.startColumnPoint; jiterator < MTObject.columnRefference + MTObject.endColumnPoint; jiterator++)
+      if (abs(iterator + jiterator - MTObject.line + 1) == order) std::cout << MTObject.matrix[iterator][jiterator] << " ";
+  std::cout << '\n';
 }
 
 template <class Type> void dataProcessor::matrixTransposition (matrixType<Type> & MTObject) {
@@ -227,19 +227,19 @@ template <class Type> void dataProcessor::matrixTransposition (matrixType<Type> 
 
 int main(int argc, char const *argv[]) {
 
-	dataProcessor __initializeProcessor__;
-	matrixType<int> MTRefference;
+ dataProcessor __initializeProcessor__;
+ matrixType<int> MTRefference;
 
   auto start = high_resolution_clock::now();
 
-	__initializeProcessor__.readMatrix ((char*)"matrix.data", MTRefference);
-	__initializeProcessor__.putsMatrix (MTRefference);
-	std::cout << '\n';
-	__initializeProcessor__.primeValuesFromMainDiagonal (MTRefference);
-	std::cout << '\n';
-	__initializeProcessor__.paralelValuesWithMD (MTRefference);
-	std::cout << '\n';
-	__initializeProcessor__.paralelValuesWithSD (MTRefference);
+ __initializeProcessor__.readMatrix ((char*)"matrix.data", MTRefference);
+ __initializeProcessor__.putsMatrix (MTRefference);
+ std::cout << '\n';
+ __initializeProcessor__.primeValuesFromMainDiagonal (MTRefference);
+ std::cout << '\n';
+ __initializeProcessor__.paralelValuesWithMD (MTRefference);
+ std::cout << '\n';
+ __initializeProcessor__.paralelValuesWithSD (MTRefference);
   std::cout << '\n';
   __initializeProcessor__.matrixTransposition (MTRefference);
   std::cout << '\n';
@@ -251,5 +251,5 @@ int main(int argc, char const *argv[]) {
 
   std::cout << "Time taken by tasks: " << duration.count() << " seconds" << '\n';
 
-	return 0;
+  return 0;
 }
