f6f6a2cddb85cfed19862ea6f886e19690af55c6
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-09 17:36:42 +0300

    Create minimumSumSequence

---
+++ minimumSumSequence/minimumSumSequenceDef.hpp
@@ -0,0 +1,78 @@
+/*
+  Tasks
+    * Get minimum sum sequence in one dimensional array
+*/
+
+#include <chrono>
+#include <fstream>
+#include <iostream>
+
+#ifndef STD_LENGTH
+#define STD_LENGTH 100000
+#endif
+
+using namespace std::chrono;
+
+class systemException : public std::exception {
+private:
+  std::string processMessage;
+
+public:
+  systemException (std::string errorMessage) : processMessage(errorMessage) {}
+
+  const char * what () const throw ();
+
+  virtual ~systemException () throw () {}
+};
+
+class validationRules {
+
+public:
+  validationRules () {}
+
+  template <class Type> bool isNegative (Type parameter);
+  template <class Type> bool isZero (Type parameter);
+
+  virtual ~validationRules () {}
+};
+
+template <class Type> class oneDimensionalArrayType {
+private:
+  int standardSize = 0;
+
+public:
+  oneDimensionalArrayType () {}
+
+  int & length = standardSize;
+  int startPoint = standardSize;
+  int endPoint = standardSize;
+
+  Type * oneDimensionalArray = new Type[STD_LENGTH];
+
+  virtual ~oneDimensionalArrayType () {}
+};
+
+class inputOutputOperations {
+private:
+  validationRules __validations__;
+
+public:
+  inputOutputOperations () {}
+
+  template <class Type> void readOneDimensionalArray (char * fileName, oneDimensionalArrayType<Type> ODARefference);
+  template <class Type> void outputOneDimensionalArray (oneDimensionalArrayType<Type> ODARefference);
+
+  virtual ~inputOutputOperations () {}
+};
+
+class getMinimumSumSequence {
+private:
+  validationRules __validations__;
+
+public:
+  getMinimumSumSequence () {}
+
+  template<class Type> Type getMinimumSumSequenceODA (oneDimensionalArrayType<Type> ODARefference);
+
+  virtual ~getMinimumSumSequence () {}
+};
