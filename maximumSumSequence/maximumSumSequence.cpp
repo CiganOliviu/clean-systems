dcb8660e9160f2e48aed4a5c99c6bd211d203db4
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-09 17:26:09 +0300

    Update maximumSumSequence

---
+++ maximumSumSequence/maximumSumSequence.cpp
@@ -0,0 +1,95 @@
+#include "maximumSumSequenceDef.hpp"
+
+const char * systemException::what () const throw () {
+
+  return processMessage.c_str();
+}
+
+template <class Type> bool validationRules::isNegative (Type parameter) {
+
+  if (parameter < 0) return true;
+
+  return false;
+}
+
+template <class Type> bool validationRules::isZero (Type parameter) {
+
+  if (parameter == 0) return true;
+
+  return false;
+}
+
+template <class Type> void inputOutputOperations::readOneDimensionalArray (char * fileName, oneDimensionalArrayType<Type> ODARefference) {
+
+  std::ifstream dataStream(fileName, std::ios::in);
+  Type data;
+
+  if (dataStream.is_open()) {
+
+    while (dataStream >> data) {
+        ODARefference.oneDimensionalArray[ODARefference.length] = data;
+        ODARefference.length += 1;
+    }
+
+    if (__validations__.isZero(ODARefference.length)) throw systemException ("Unable to process length as zero");
+    if (__validations__.isNegative(ODARefference.length)) throw systemException ("Unable to process negative length");
+
+    dataStream.close();
+  }
+  else
+    throw systemException ("Unable to open file");
+}
+
+template <class Type> void inputOutputOperations::outputOneDimensionalArray (oneDimensionalArrayType<Type> ODARefference) {
+
+  if (__validations__.isZero(ODARefference.length)) throw systemException ("Unable to process length as zero");
+  if (__validations__.isNegative(ODARefference.length)) throw systemException ("Unable to process negative length");
+
+  for (size_t iterator = ODARefference.startPoint; iterator < ODARefference.length + ODARefference.endPoint; iterator++)
+    std::cout << ODARefference.oneDimensionalArray[iterator] << " ";
+  std::cout << '\n' << '\n' << '\n';
+}
+
+template <class Type> Type getMaximumSumSequence::getMaximumSumSequenceODA (oneDimensionalArrayType<Type> ODARefference) {
+
+  if (__validations__.isZero(ODARefference.length)) throw systemException ("Unable to process length as zero");
+  if (__validations__.isNegative(ODARefference.length)) throw systemException ("Unable to process negative length");
+
+  Type Sum = 0;
+  Type Result = 0;
+
+  for (size_t iterator = ODARefference.startPoint; iterator < ODARefference.length + ODARefference.endPoint; iterator++) {
+
+    Sum += ODARefference.oneDimensionalArray[iterator];
+
+    if (Result < Sum)
+      Result = Sum;
+
+    if (Sum < 0)
+      Sum = 0;
+  }
+
+  return Result;
+}
+
+int main(int argc, char const *argv[]) {
+
+  inputOutputOperations io;
+  oneDimensionalArrayType<int> ODAObject;
+  getMaximumSumSequence SumSequenceCore;
+
+  auto start = high_resolution_clock::now();
+
+  io.readOneDimensionalArray<int> ((char*)"ODA.data", ODAObject);
+  io.outputOneDimensionalArray<int> (ODAObject);
+
+  std::cout << SumSequenceCore.getMaximumSumSequenceODA<int> (ODAObject) << '\n' << '\n';
+
+  auto stop = high_resolution_clock::now();
+
+  auto duration = duration_cast<seconds>(stop - start);
+
+  std::cout << "Time taken by tasks: " << duration.count() << " seconds" << '\n';
+
+  return 0;
+}
