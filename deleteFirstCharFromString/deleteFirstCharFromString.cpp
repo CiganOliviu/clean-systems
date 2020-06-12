5100746abcb89d0ec3c9676b97b3406213aa7377
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-06-12 21:51:45 +0300

    Create deleteFirstCharFromString

---
+++ deleteFirstCharFromString/deleteFirstCharFromString.cpp
@@ -0,0 +1,85 @@
+#include "deleteFirstCharFromStringDef.hpp"
+
+const char * systemException::what () const throw () {
+
+  return processMessage.c_str();
+}
+
+bool validationRules::isNegative (char parameter[]) {
+
+  if (strlen(parameter) < 0) return true;
+
+  return false;
+}
+
+bool validationRules::isZero (char parameter[]) {
+
+  if (strlen(parameter) == 0) return true;
+
+  return false;
+}
+
+void inputOutputSystem::readOneDimensionalArray (oneDimensionalArrayType<char> dataWorkFlow) {
+
+  if (__validations__.isZero(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process length as zero");
+  if (__validations__.isNegative(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process negative length");
+
+  std::cin.getline (dataWorkFlow.oneDimensionalArray, dataWorkFlow.length);
+
+}
+
+void inputOutputSystem::outputOneDimensionalArray (oneDimensionalArrayType<char> dataWorkFlow) {
+
+  if (__validations__.isZero(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process length as zero");
+  if (__validations__.isNegative(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process negative length");
+
+  std::cout << dataWorkFlow.oneDimensionalArray << '\n';
+}
+
+char stringWorkFlow::saveTermToDelete (oneDimensionalArrayType<char> dataWorkFlow) {
+
+  char result;
+
+  result = dataWorkFlow.oneDimensionalArray[0];
+
+  return result;
+}
+
+void stringWorkFlow::deleteTermFromString (oneDimensionalArrayType<char> dataWorkFlow) {
+
+  if (__validations__.isZero(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process length as zero");
+  if (__validations__.isNegative(dataWorkFlow.oneDimensionalArray)) throw systemException ("Unable to process negative length");
+
+  char term = saveTermToDelete (dataWorkFlow);
+
+  for (int iterator = dataWorkFlow.startPoint; iterator < strlen(dataWorkFlow.oneDimensionalArray); iterator++)
+    if (dataWorkFlow.oneDimensionalArray[iterator] == term) {
+      strcpy (dataWorkFlow.oneDimensionalArray + iterator, dataWorkFlow.oneDimensionalArray + iterator + 1);
+      iterator -= 1;
+    }
+}
+
+int main (int argc, char const *argv[]) {
+
+  oneDimensionalArrayType<char> string;
+  inputOutputSystem IOSystem;
+  stringWorkFlow workflow;
+
+  string.length = 100;
+
+  IOSystem.readOneDimensionalArray (string);
+
+  auto start = high_resolution_clock::now();
+
+  workflow.deleteTermFromString (string);
+
+  IOSystem.outputOneDimensionalArray (string);
+  
+  auto stop = high_resolution_clock::now();
+
+  auto duration = duration_cast<seconds>(stop - start);
+
+  std::cout << "Time taken by tasks: " << duration.count() << " seconds" << '\n';
+
+  return 0;
+}
